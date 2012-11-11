4863525143ad44528f0fa00062c32f42520a0284
cantwt <cantwt22@gmail.com>
2012-11-10 19:27:51 -0500

    Base Threading Implementation
    Implemented the threading necessary for output to be sent on the serial
    line, and additionally to take incoming data from the input buffer and
    display it on the main text output window.

--- Terblonix/Terblonix/terblonix.py
+++ Terblonix/Terblonix/terblonix.py
@@ -1,16 +1,20 @@
 import clr
+clr.AddReference("IronPython")
 clr.AddReference("PresentationFramework")
 clr.AddReference("PresentationCore")
+clr.AddReference("WindowsBase")
 
+from IronPython.Compiler import CallTarget0
 from System.IO import File
 from System.IO.Ports import SerialPort
 from System.Windows.Markup import XamlReader
+from System.Windows.Threading import Dispatcher, DispatcherPriority
 
 from System.Windows import (
     Application, Window
 )
 from System.Windows.Controls import (
-    Label, ComboBox, ComboBoxItem
+    Label, ComboBox, ComboBoxItem, TextBox
 )
 
 class MainWin(object):
@@ -39,6 +43,13 @@ class MainWin(object):
         self.dockbar = self.Root.FindName('dockbar')
         self.dockbar.MouseLeftButtonDown += self.dragDock
 
+        self.inputBuff = self.Root.FindName('inputBox')
+
+        self.outputBuff = self.Root.FindName('outputBox')
+
+        self.send = self.Root.FindName('send')
+        self.send.Click += self.sendClick
+
     def scanClick(self, sender, event):
         self.combo.Items.Clear()
         self.ports = SerialPort.GetPortNames()
@@ -53,11 +64,28 @@ class MainWin(object):
     def connectClick(self, sender, event):
         if self.connect.Content == 'Connect':
             self.connect.Content = 'Disconnect'
+            self.term = SerialPort(self.combo.SelectedValue.Content, 9600)  # Set to 9600 for calibration purposes because that is what my board uses
+            self.term.Open()
+            self.term.DataReceived += self.dataRX
         else:
             self.connect.Content = 'Connect'
+            self.term.Close()
 
     def dragDock(self, sender, event):
         self.window.DragMove()
+    
+    def sendClick(self, sender, event):
+        self.term.Write(self.inputBuff.Text)
+        self.inputBuff.Text = ''
+
+    def dataRX(self, sender, event):
+        tempText = self.term.ReadExisting()
+        textDelegate = self.outDel(tempText)
+        self.outputBuff.Dispatcher.Invoke(DispatcherPriority.Normal, textDelegate)
+
+    def outDel(self, text):
+        return CallTarget0(lambda: TextBox.AppendText(self.outputBuff, text + '\n'))
+
 
 stage = MainWin()
 
