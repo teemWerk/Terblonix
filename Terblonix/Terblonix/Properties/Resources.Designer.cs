76787f2b0b84d55e73d22f00e301d4020ce0ca05
cantwt <cantwt22@gmail.com>
2012-11-13 03:42:26 -0500

    Application Icon
    Officially Added Terblonix to the *blonix family with the thievery of
    the Gerblonix logo.

--- Terblonix/Terblonix/Properties/Resources.Designer.cs
+++ Terblonix/Terblonix/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace Terblonix.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon g48 {
+            get {
+                object obj = ResourceManager.GetObject("g48", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap max {
